fd81345ea482bfa089dba5acff8bd3a979882337
Juan Fonseca <juan.fonseca@gorillalogic.com>
2020-03-30 12:21:44 -0600

    Updated install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,13 @@
 binFolder=~/Library/Python/3.7/bin/
 chromeZip=chrome.zip
 venvFolder=./test_automation
+chromeVersion=80.0.3987.106
 
 export PATH=$binFolder:$PATH
 virtualenv $venvFolder
 source $venvFolder/bin/activate
 pip install -r requirements.txt
-curl -o $chromeZip https://chromedriver.storage.googleapis.com/80.0.3987.106/chromedriver_mac64.zip
+curl -o $chromeZip https://chromedriver.storage.googleapis.com/$chromeVersion/chromedriver_mac64.zip
 unzip $chromeZip
 mv chromedriver $binFolder
 behave
