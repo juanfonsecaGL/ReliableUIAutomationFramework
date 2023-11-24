5a88670d903ccf79587bdcce7305de8c18b0af6b
Juan Fonseca <jfonseca@firstam.com>
2023-11-24 10:28:29 -0600

    Updated scripts.

--- part3/run.sh
+++ part3/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-binFolder=~/Library/Python/3.7/bin/
+#binFolder=~/Library/Python/3.7/bin/
 venvFolder=../.test_automation
 
-export PATH=$binFolder:$PATH
-source $venvFolder/bin/activate
+#export PATH=$binFolder:$PATH
+source $venvFolder/Scripts/activate
 behave
