ea8242e327b43c570f8491a5ef482e2a6a32a807
Juan Fonseca <jfonseca@firstam.com>
2023-11-24 10:25:45 -0600

    Updated scripts.

--- install.sh
+++ install.sh
@@ -3,16 +3,19 @@
 binFolder=~/Library/Python/3.7/bin/
 chromeZip=chrome.zip
 venvFolder=.test_automation
-chromeVersion=80.0.3987.106
+#chromeVersion=119.0.6045.160
+
+pip install virtualenv
 
 export PATH=$binFolder:$PATH
-virtualenv $venvFolder
-source $venvFolder/bin/activate
+python -m virtualenv $venvFolder
+#virtualenv $venvFolder
+source $venvFolder/Scripts/activate
 pip install -r requirements.txt
-curl -o $chromeZip https://chromedriver.storage.googleapis.com/$chromeVersion/chromedriver_mac64.zip
-unzip $chromeZip
-mv chromedriver $binFolder
-rm $chromeZip
+#curl -o $chromeZip https://chromedriver.storage.googleapis.com/$chromeVersion/chromedriver_mac64.zip
+#unzip $chromeZip
+#mv chromedriver $binFolder
+#rm $chromeZip
 
 echo
-echo "FINISHED!!!"
+#echo "FINISHED!!!"
